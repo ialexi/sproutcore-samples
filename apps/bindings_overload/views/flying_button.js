4f2eb7240add0dfe50128ee08dbacdd0c65f00b4
Alex Iskander <alex@tpsitulsa.com>
2009-12-22 16:54:18 -0600

    Make it use SC.Animatable.

--- apps/bindings_overload/views/flying_button.js
+++ apps/bindings_overload/views/flying_button.js
@@ -14,9 +14,22 @@
 
   @extends SC.View
 */
-BindingsOverload.FlyingButtonView = SC.ButtonView.extend(
+BindingsOverload.FlyingButtonView = SC.ButtonView.extend(SC.Animatable,
 /** @scope BindingsOverload.FlyingButtonView.prototype */ {
-
+  /**
+  Set up some transitions and initial styles.
+  Keep in mind we'll be pooling/unpooling, so we'll have to reset each time.
+  */
+  transitions: {
+    left: { duration: 1, timing: SC.Animatable.TRANSITION_EASE_IN_OUT },
+    top: { duration: 1, timing: SC.Animatable.TRANSITION_EASE_IN_OUT },
+    opacity: { duration: 1, timing: SC.Animatable.TRANSITION_EASE_IN_OUT }
+  },
+  
+  style: {
+    opacity: 1
+  },
+  
   /**
     Choose a random position in the viewport and move the view there.
 
@@ -24,11 +37,6 @@ BindingsOverload.FlyingButtonView = SC.ButtonView.extend(
     rendered and placed in the DOM.
   */
   didAppendToDocument: function() {
-    // Perform the repositioning in 100ms.
-    // This is necessary due to an apparent optimization in WebKit that will
-    // just move the element to the new location instead of triggering an
-    // optimization.
-
     // invokeLater is similar to setTimeout, but is more performant and
     // reliable. It is available on any instance of SC.Object.
     this.invokeLater(function() {
@@ -50,14 +58,12 @@ BindingsOverload.FlyingButtonView = SC.ButtonView.extend(
     fade out.
   */
   dimControl: function() {
-    // $() is a function of SC.Views that returns a CoreQuery object.
-    // CoreQuery objects allow you to directly manipulate the DOM in a
-    // cross-platform-safe way.  You should always use CQ objects instead of
-    // native DOM elements.
-    this.$().css('opacity',0);
-
+    // the animation mixin adds style manipulation abilities to adjust,
+    // allowing us to change opacity (and, better, animate it)
+    this.adjust({"opacity": 0}).updateStyle();
+    
     // Remove the button after the fade completes.
-    this.invokeLater(this.removeButton, 1000);
+    this.invokeLater(this.removeButton, 1200);
   },
 
   /**
@@ -65,8 +71,10 @@ BindingsOverload.FlyingButtonView = SC.ButtonView.extend(
     and returns the button to the button pool.
   */
   removeButton: function() {
+    this.adjust({"opacity": 1}).updateStyle(); // quick! set it before we remove the layer!
+    //this.transitions.top.duration = this.transitions.left.duration = 1;
+    
     this.removeFromParent();
-    this.$().css('opacity',1);
     BindingsOverload.appController.returnToPool(this);
   },
 
@@ -77,8 +85,7 @@ BindingsOverload.FlyingButtonView = SC.ButtonView.extend(
     var rand = Math.floor(Math.random()*400+100), x, y,
         frame = this.get('frame');
 
-    this.$().css('-webkit-transition-duration', '0.2s, 0.2s, 1s');
-    this.$().css('-webkit-transition-timing-function', 'linear, linear, linear');
+    //this.transitions.top.duration = this.transitions.left.duration = 0.2;
     x = (Math.random() > 0.5) ? rand : rand * -1;
     y = (Math.random() > 0.5) ? rand : rand * -1;
     this.adjust({ top: y, left: x });
